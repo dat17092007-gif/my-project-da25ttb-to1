d3a4d37f946983cd27978bbbef8ac6f5578605f1
Ba Dat <dat17092007@gmail.com>
2026-06-11 10:44:10 +0700

    nguyendat

--- js/lib.js
+++ js/lib.js
@@ -1,1 +1,14 @@
-nbd
+/*Viết mã Javascript*/
+function printBCC(num)
+{
+    let result = "";
+
+    let i = 1;
+    while(i<=10)
+    {
+        result += `${num} x ${i} = ${num*i} <br>`; 
+        i++;
+    }
+    //Đưa kết quả vào thẻ div có id là "result"
+    document.getElementById("result").innerHTML = result;
+}
